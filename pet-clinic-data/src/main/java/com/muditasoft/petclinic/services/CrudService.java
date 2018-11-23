297ffb9d5f8abfcd4c7d0e1baad586d7a2eceb1f
tutkuince <tutku.ince@outlook.com>
2018-11-23 19:54:18 +0300

    Refactor Service Interface to Common Base interface. Closes #11

---
+++ pet-clinic-data/src/main/java/com/muditasoft/petclinic/services/CrudService.java
@@ -0,0 +1,20 @@
+package com.muditasoft.petclinic.services;
+
+import java.util.Set;
+
+/**
+ * @author tutkuince
+ * Date 11/23/2018
+ */
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+}
--- pet-clinic-data/src/main/java/com/muditasoft/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/muditasoft/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.muditasoft.petclinic.services;
 
 import com.muditasoft.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
